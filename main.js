1405fa1b9917fee0ba82205926a660945d7f9052
corvofeng <corvofeng@gmail.com>
2024-05-07 10:46:49 +0800

    Update the proxy.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- main.js
+++ main.js
@@ -41,6 +41,8 @@ function createWindow() {
   mainWindow.webContents.session
     .setProxy({
       // proxyRules:"socks5://54.248.22.243:1080",
+      // proxyRules:"socks5://192.168.191.201:8080",
+      proxyRules:"http://192.168.191.201:8080",
       // proxyRules:"http://127.0.0.1:1080",
     }).then(() => {
       mainWindow.loadURL('https://chat.openai.com/');
